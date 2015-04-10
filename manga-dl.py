8c804c535a06afb1ba696912c557603ef0e8d4c4
Makoto <FujiMakoto@users.noreply.github.com>
2015-04-10 11:38:35 -0400

    Debug logging

--- manga-dl.py
+++ manga-dl.py
@@ -1,4 +1,5 @@
 #!/bin/env python3
+import logging
 from src.config import Config
 from src.cli import CLI
 
@@ -6,6 +7,17 @@ from src.cli import CLI
 def main():
     config = Config()
     cli = CLI()
+
+    # Set up logging
+    log = logging.getLogger('manga-dl')
+    log_formatter = logging.Formatter("[%(asctime)s] %(levelname)s.%(name)s: %(message)s")
+
+    # Set up our console logger
+    log.setLevel(logging.DEBUG)
+    console_logger = logging.StreamHandler()
+    console_logger.setLevel(logging.DEBUG)
+    console_logger.setFormatter(log_formatter)
+    log.addHandler(console_logger)
 
     # If this is our first time running the application, run setup first
     if not config.app_config_exists():
